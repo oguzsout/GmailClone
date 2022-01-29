2815546bac7a3f14b8301fa5af3d4be1bb0a0130
oguzsout <oguzd494@gmail.com>
2022-01-29 18:07:18 +0300

    created dialog UI and preview

--- app/src/main/java/com/oguzdogdu/gmailclone/components/AccountsDialog.kt
+++ app/src/main/java/com/oguzdogdu/gmailclone/components/AccountsDialog.kt
@@ -1,17 +1,65 @@
 package com.oguzdogdu.gmailclone.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import com.oguzdogdu.gmailclone.R
 
 @Composable
 fun AccountsDialog(openDialog: MutableState<Boolean>) {
     Dialog(onDismissRequest = { openDialog.value = false }) {
-        Card(backgroundColor = Color.White) {
-            Text(text = "Account")
+        AccountsDialogUI()
+    }
+}
+
+@Composable
+fun AccountsDialogUI() {
+    Card {
+        Column(
+            modifier = Modifier
+                .background(Color.White)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = ""
+                    )
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.google), contentDescription = "",
+                    modifier = Modifier
+                        .size(30.dp)
+                        .weight(2.0f)
+                )
+
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun AccountsDialogUiPreview() {
+    AccountsDialogUI()
 }
