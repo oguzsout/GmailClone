0a686cb3c90e95d762ef4483f37bf40b19b1082d
oguzsout <oguzd494@gmail.com>
2022-01-29 18:25:13 +0300

    add the current account with Google Account button and Divider

--- app/src/main/java/com/oguzdogdu/gmailclone/components/AccountsDialog.kt
+++ app/src/main/java/com/oguzdogdu/gmailclone/components/AccountsDialog.kt
@@ -1,26 +1,28 @@
 package com.oguzdogdu.gmailclone.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.oguzdogdu.gmailclone.R
+import com.oguzdogdu.gmailclone.ui.theme.CustomFont
 
 @Composable
 fun AccountsDialog(openDialog: MutableState<Boolean>) {
@@ -30,7 +32,7 @@ fun AccountsDialog(openDialog: MutableState<Boolean>) {
 }
 
 @Composable
-fun AccountsDialogUI() {
+fun AccountsDialogUI(modifier: Modifier = Modifier) {
     Card {
         Column(
             modifier = Modifier
@@ -54,6 +56,51 @@ fun AccountsDialogUI() {
                 )
 
             }
+            Row(
+                modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.gmail_profile),
+                    contentDescription = "Profile",
+                    modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(color = Color.Gray)
+                )
+                Column(
+                    modifier
+                        .weight(2.0f)
+                        .padding(start = 16.dp, bottom = 16.dp)
+                ) {
+                    Text(
+                        text = "Jacky Shepherd",
+                        fontWeight = FontWeight.SemiBold,
+                        fontFamily = CustomFont
+                    )
+                    Text(text = "jackyshepherd@gmail.com", fontFamily = CustomFont)
+                }
+                Text(text = "99+", modifier = Modifier.padding(end = 16.dp))
+            }
+            Row(
+                modifier.fillMaxWidth(), horizontalArrangement =
+                Arrangement.SpaceEvenly
+            ) {
+                Card(
+                    modifier.requiredWidth(150.dp),
+                    shape = RoundedCornerShape(50.dp),
+                    border = BorderStroke(1.dp, color = Color.Gray)
+                ) {
+                    Text(
+                        text = "Google Account",
+                        modifier.padding(8.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Spacer(modifier.width(10.dp))
+            }
+            Divider(modifier.padding(top = 16.dp))
         }
     }
 }
